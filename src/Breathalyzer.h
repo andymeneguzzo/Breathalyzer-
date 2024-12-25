a938720b058a38593c61d4dc36f2a5b9e7f8b441
Andy <andres.meneguzzo@gmail.com>
2024-12-25 01:09:09 +0100

    Breathalyzer.h created

---
+++ src/Breathalyzer.h
@@ -0,0 +1,28 @@
+#ifndef BREATHALYZIER_H
+#define BREATHALYZIER_H
+
+#include <vector>
+#include "Drink.h"
+#include "Meal.h"
+
+class Breathalyzer {
+    private:
+        std::vector<Drink> drinks;
+        std::vector<Meal> meals;
+        double userWeight;;
+        bool reduceForMeals;
+
+    public:
+        Breathalyzer(double userWeight = 70.0); // default peso
+
+        void addDrink(const Drink& drink);
+        void addMeal(const Meal& meal);
+        void setUserWeight(double userWeight);
+
+        // Metodo PRINCIPALE per calcolo BAC
+        double calculateBAC();
+
+        static std::string getSanzioniFromBAC(double bac);
+};
+
+#endif
